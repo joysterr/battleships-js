f2a698f2dd8ad140590e653fe1564c9570311770
Joyster Rodrigues <77756392+joysterr@users.noreply.github.com>
2024-02-20 22:58:04 +0000

    update gui styling and board.js add grid layout fix board.js button loop added css variables

--- scripts/board.js
+++ scripts/board.js
@@ -4,9 +4,8 @@ const board = document.querySelector('#board')
 function createBoard() {
     for (i = 0; i < grid; i++) {
         for (j = 0; j < grid; j++) {
-            board.innerHTML += `<button id="${i}${j}" class="board-btn" onclick="fire(${i},${j})">~</button>`
+            board.innerHTML += `<button id="${i}${j}" class="board__btn" onclick="fire(${i},${j})">~</button>`
         }
-        board.innerHTML += "<br>"
     }
 }
 
