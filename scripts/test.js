d91ffc12034c8dd9e6e55a4c231933eaf93bccb1
Joyster Rodrigues <77756392+joysterr@users.noreply.github.com>
2024-02-20 23:25:35 +0000

    add styling for missed shot and minor fixes change board btn id added css

--- scripts/test.js
+++ scripts/test.js
@@ -1,3 +1,7 @@
 function fire(xCo, yCo) {
     console.log(xCo, yCo)
+    const btnClicked = document.getElementById(`btn${xCo}${yCo}`)
+    console.log(btnClicked.id)
+    btnClicked.innerHTML = '&times;'
+    btnClicked.classList.add('board__btn--miss')
 }
