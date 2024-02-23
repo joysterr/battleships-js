40cb1431e8813fe630b5aee7ddf4b815eee0cf2e
Joyster Rodrigues <77756392+joysterr@users.noreply.github.com>
2024-02-23 12:56:30 +0000

    fix: load screen should appear only on first load of page

--- scripts/gui.js
+++ scripts/gui.js
@@ -1,3 +1,5 @@
+const instructionsMenu = document.querySelector('.overlay-screen')
+
 function loadInstructionTable() {
     const shipTable = document.getElementById('shipTableData')
 
@@ -14,9 +16,23 @@ function loadInstructionTable() {
 
 function hideScreen(num) {
     if (num === 0) {
-        const instructionsMenu = document.querySelector('.overlay-screen')
         instructionsMenu.style.top = '-100%'
     }
 }
 
+function showScreen(num) {
+    if (num === 0) {
+        instructionsMenu.style.top = '0'
+    }
+}
+
+// load instrcution menu only on first load
+function initInstructionMenu() {
+    if (!localStorage.getItem('first-load')) {
+        showScreen(0)
+        localStorage.setItem('first-load', true)
+    }
+}
+
 loadInstructionTable()
+initInstructionMenu()
